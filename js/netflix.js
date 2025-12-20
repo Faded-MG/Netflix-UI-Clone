7b527722f212ade3c7de7dc63a696e5744770a1b
FADED <mahletgetinet17@gmail.com>
2025-12-20 20:46:05 +0300

    Initial commit: add Netflix clone project files

---
+++ js/netflix.js
@@ -0,0 +1,74 @@
+const what = document.querySelectorAll(".what");
+
+what.forEach(item => {
+  const question = item.querySelector(".what-is-net");
+  const icon = item.querySelector(".icon");
+
+  question.addEventListener("click", () => {
+    item.classList.toggle("active");
+    icon.textContent = item.classList.contains("active") ? "x" : "+";
+  });
+});
+
+const how = document.querySelectorAll(".how");
+
+how.forEach(item => {
+  const question = item.querySelector(".howMuch");
+  const icon = item.querySelector(".icon");
+
+  question.addEventListener("click", () => {
+    item.classList.toggle("active");
+    icon.textContent = item.classList.contains("active") ? "x" : "+";
+  });
+});
+ 
+
+const where = document.querySelectorAll(".where");
+
+where.forEach(item => {
+  const question = item.querySelector(".whereCan");
+  const icon = item.querySelector(".icon");
+
+  question.addEventListener("click", () => {
+    item.classList.toggle("active");
+    icon.textContent = item.classList.contains("active") ? "x" : "+";
+  });
+});
+
+const cancel = document.querySelectorAll(".cancel");
+
+cancel.forEach(item => {
+  const question = item.querySelector(".cancelHow");
+  const icon = item.querySelector(".icon");
+
+  question.addEventListener("click", () => {
+    item.classList.toggle("active");
+    icon.textContent = item.classList.contains("active") ? "x" : "+";
+  });
+});
+
+
+const watch = document.querySelectorAll(".watch");
+
+watch.forEach(item => {
+  const question = item.querySelector(".watchWhat");
+  const icon = item.querySelector(".icon");
+
+  question.addEventListener("click", () => {
+    item.classList.toggle("active");
+    icon.textContent = item.classList.contains("active") ? "x" : "+";
+  });
+});
+
+
+const kids = document.querySelectorAll(".kids");
+
+kids.forEach(item => {
+  const question = item.querySelector(".kidsNet");
+  const icon = item.querySelector(".icon");
+
+  question.addEventListener("click", () => {
+    item.classList.toggle("active");
+    icon.textContent = item.classList.contains("active") ? "x" : "+";
+  });
+});
